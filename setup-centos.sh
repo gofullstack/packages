e209554d7d2724b3187503d7a0ab16c0a48166e6
root <root@ip-10-114-19-2.ec2.internal>
2011-02-15 01:50:39 +0000

    Add openssl-devel

--- setup-centos.sh
+++ setup-centos.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 yum -y groupinstall 'Development Tools'
-yum -y install rpmdevtools readline-devel
+yum -y install rpmdevtools readline-devel openssl-devel
 
 rpmdev-setuptree
 cp SPECS/ruby-enterprise.spec ~/rpmbuild/SPECS/
