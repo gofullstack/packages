03c27777a0b2a2ecb6cec230b2ef3d2b038d50c6
Nathan L Smith <nlloyds@gmail.com>
2011-10-24 08:50:01 -0500

    reorg

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd ~/rpmbuild
-QA_RPATHS="0xFFFF" rpmbuild -ba SPECS/ruby-enterprise.spec
-
--- setup-centos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-yum -y groupinstall 'Development Tools'
-yum -y install rpmdevtools readline-devel openssl-devel
-
-rpmdev-setuptree
-ln -s ~/ruby-enterprise-rpm/SPECS/ruby-enterprise.spec ~/rpmbuild/SPECS/
-ln -s ~/ruby-enterprise-rpm/SOURCES/ruby-enterprise-1.8.7-2010.02.tar.gz ~/rpmbuild/SOURCES/
-
